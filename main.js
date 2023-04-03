bf54cfe579903acb070b60b6591232725d3db48f
danieljuma5 <jumadaniel275@gmail.com>
2023-04-04 01:59:09 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -2,10 +2,48 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+// Adding event listener for when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('modal').classList.add('hidden')
+})
+//Event listeners for all empty hearts
+const emptyHeart = document.getElementsByClassName('like-glyph');
+for(element of emptyHeart) {
+  element.addEventListener('click', (e) => {
+    const heart = e.target
+    mimicServerCall()
+    .then(() => {
+      element = heart
+      element.innerHTML = FULL_HEART
+      element.classList.add("activated-heart")
+      element.classList.remove("like-glyph")
 
-
-
+      
+      
+    })
+    .catch((error) => {
+      alert("Random server error. Try again.")
+      document.getElementById('modal').classList.remove('hidden')
+      document.getElementById('modal').innerHTML = error
+  
+  
+      setTimeout(function() {
+        document.getElementById('modal').classList.add('hidden')
+        
+  
+      },3000)
+      console.log(error.message)
+    })
+    })
+  }
+//When a user clicks on a full heart
+document.addEventListener("click", (e) => {   
+      const heart = e.target
+      element = heart
+      element.innerHTML = EMPTY_HEART
+      element.classList.remove("activated-heart")
+      element.classList.add("like-glyph")
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
